05cbf30f6a8e8dd262c8a1f0025278eb5324fa96
Rfontt <ritadecassia.infor2@gmail.com>
2024-06-03 20:46:51 -0300

    feat: add deque methods

--- deque/main.cpp
+++ deque/main.cpp
@@ -40,12 +40,117 @@ struct Deque {
                 head = newNode;
             }
         }
+
+        void pushBack(int value) {
+            Node* newNode = new Node(value);
+
+            if (isEmpty()) {
+                head = tail = newNode;
+            } else {
+                newNode->previous = tail;
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+
+        void forwardTraversal() {
+            Node* current = head;
+
+            while (current != nullptr) {
+                cout << current->data << "-->";
+
+                current = current->next;
+            }
+
+            cout << "nullptr" << endl;
+        }
+
+        void backwardTravesal() {
+            Node* current = tail;
+
+            while (current != nullptr) {
+                cout << current->data << "-->";
+
+                current = current->previous;
+            }
+
+            cout << "nullptr" << endl;
+        }
+
+        void popFront() {
+            if (isEmpty()) {
+                throw runtime_error("Deque is empty");
+            }
+
+            Node* temp = head;
+
+            head = head->next;
+
+            if(head != nullptr) {
+                head->previous = nullptr;
+            } else {
+                tail = nullptr;
+            }
+
+            delete temp;
+        }
+
+        void popBack() {
+            if (isEmpty()) {
+                throw runtime_error("Deque is empty");
+            }
+
+            Node* temp = tail;
+
+            tail = tail->previous;
+
+            if(tail != nullptr) {
+                tail->next = nullptr;
+            } else {
+                head = nullptr;
+            }
+
+            delete temp;
+        }
+
+        int getFirst() {
+            if (isEmpty()) {
+                throw runtime_error("Deque is empty");
+            }
+
+            return head->data;
+        }
+
+        int getLast() {
+            if (isEmpty()) {
+                throw runtime_error("Deque is empty");
+            }
+
+            return tail->data;
+        }
 };
 
 int main() {
     Deque deque;
 
     deque.pushFront(10);
+    deque.pushFront(20);
+    deque.pushBack(30);
+    deque.pushBack(300);
 
+    deque.forwardTraversal();
+    deque.backwardTravesal();
+
+    deque.popFront();
+
+    deque.forwardTraversal();
+
+    deque.popBack();
+
+    deque.backwardTravesal();
+
+    cout << "first: " << deque.getFirst() << endl;
+    cout << "last: " << deque.getLast() << endl;
+    
     return 0; 
 }
